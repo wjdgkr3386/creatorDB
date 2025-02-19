2f39d452bf56fa65a71581d1661342525c0325f0
Jeonghak Kim <wjdgkr3386@naver.com>
2025-02-19 17:18:25 +0900

    프로젝트 폴더 깃허브에 연동

---
+++ creatorDB/src/main/java/com/creatorDB/demo/CreatorDbApplication.java
@@ -0,0 +1,13 @@
+package com.creatorDB.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorDbApplication.class, args);
+	}
+
+}
---
+++ creatorDB/src/test/java/com/creatorDB/demo/CreatorDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creatorDB.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
